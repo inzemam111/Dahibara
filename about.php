b51ea758e49f77090fcc9c762371c341f2e374a9
Md Inzemamul Haque <mdinzemamulh11@gmail.com>
2024-07-31 16:26:49 +0530

    First_Commit

---
+++ about.php
@@ -0,0 +1,473 @@
+<?php include 'header.php'; ?>
+
+<div class="hero-section">
+    <!-- <img src="../assets/image.png" alt="About Us" class="hero-image"> -->
+    <img src="./assets/about/Hero.png" alt="About Us" class="hero-image">
+
+    <div class="hero-text">Get to know our story</div>
+</div>
+
+<!-- Next Section [Section 2] -->
+
+<section class="about-us-2">
+    <div class="text-container-2">
+        <h2>About us</h2>
+        <p>Established in 2022, Dahibara Express redefines traditional Odia cuisine with a modern twist. Originating from our observation that traditional dishes lacked high-quality accessibility, we aimed to fill this gap. Based in Bhubaneswar, our menu boasts a variety of dishes crafted with premium ingredients and a blend of innovative and traditional cooking techniques. We continuously expand our menu and techniques to offer the most exciting flavors. Committed to providing an exceptional customer experience, our friendly and knowledgeable staff is eager to assist you. Thank you for choosing Dahibara Express; we look forward to serving you soon!</p>
+        <div>
+            <section class="statistics">
+                <div class="stat">
+                    <h3>40+</h3>
+                    <p>Outlet</p>
+                </div>
+                <div class="stat">
+                    <h3>10+</h3>
+                    <p>Cities</p>
+                </div>
+                <div class="stat">
+                    <h3>1 Lac+</h3>
+                    <p>Orders</p>
+                </div>
+            </section>
+
+        </div>
+
+    </div>
+    <div class="image-container">
+        <img src="./assets/about/map.png" alt="Map" class="map-image">
+    </div>
+</section>
+
+
+<!-- Next Section [Section 3] -->
+<div class="section-header">
+    <h1>Core Team</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia voluptatibus asperiores cum repellat dolores. Exercitationem praesentium repellendus ipsam iure animi!</p>
+</div>
+
+
+
+<main class="page-content">
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core ">Sourabh Khandelwal</h2>
+            <p class="copy color_3">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Sunil Kota</h2>
+            <p class="copy color_3">Plan your next beach trip with these fabulous destinations</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Dr. Sushant Ranjan Chaini</h2>
+            <p class="copy color_3">It's the desert you've always dreamed of</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Mr. Jashwant Pardhi</h2>
+            <p class="copy color_3">Seriously, straight up, just blast off into outer space today</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Mr. Jashwant Pardhi</h2>
+            <p class="copy color_3">Seriously, straight up, just blast off into outer space today</p>
+        </div>
+    </div>
+</main>
+
+
+<!-- Next Section [Section 4] -->
+
+<div class="section-header-at">
+    <h1>Associate Team</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia voluptatibus asperiores cum repellat dolores. Exercitationem praesentium repellendus ipsam iure animi!</p>
+</div>
+
+
+
+<main class="page-content-at">
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core ">Sourabh Khandelwal</h2>
+            <p class="copy color_3">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Sunil Kota</h2>
+            <p class="copy color_3">Plan your next beach trip with these fabulous destinations</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Dr. Sushant Ranjan Chaini</h2>
+            <p class="copy color_3">It's the desert you've always dreamed of</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title_core">Mr. Jashwant Pardhi</h2>
+            <p class="copy color_3">Seriously, straight up, just blast off into outer space today</p>
+        </div>
+    </div>
+
+</main>
+
+
+
+<!-- NEXT SECTION 6  -->
+<div class="section-header-at">
+    <h1>Awards & Recognitions</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia voluptatibus asperiores cum repellat dolores. Exercitationem praesentium repellendus ipsam iure animi!</p>
+</div>
+
+<div class="image-gallery">
+
+        <div class="gallery-item">
+            <img src="./assets/about/award/1.png" alt="Description 1">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/2.png" alt="Description 2">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/3.png" alt="Description 3">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/4.png" alt="Description 4">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/5.png" alt="Description 5">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/6.png" alt="Description 6">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/7.png" alt="Description 7">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/award/8.png" alt="Description 8">
+        </div>
+  
+
+</div>
+
+
+<!-- NEXT SECTION 7  -->
+
+<div class="section-header-at">
+    <h1>Events</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia voluptatibus asperiores cum repellat dolores. Exercitationem praesentium repellendus ipsam iure animi!</p>
+</div>
+
+<div class="image-gallery">
+
+    
+        <div class="gallery-item">
+            <img src="./assets/about/Events/1.png" alt="Description 1">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/2.png" alt="Description 2">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/3.png" alt="Description 3">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/4.png" alt="Description 4">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/5.png" alt="Description 5">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/6.png" alt="Description 6">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/7.png" alt="Description 7">
+        </div>
+        <div class="gallery-item">
+            <img src="./assets/about/Events/8.png" alt="Description 8">
+        </div>
+
+</div>
+
+
+
+<!-- Start Published on 
+    ============================================= -->
+    <div>
+    <section class="published-on">
+        <div class="container site-heading">
+            <h2>Published on</h2>
+            <div class="row justify-content-center pub_pad">
+
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/1.png" alt="The Times of India" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/2.png" alt="Zee Media" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/3.png" alt="ANI News India" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/4.png" alt="Indian Express" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/5.png" alt="Business Standard" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/6.png" alt="Jio TV" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/7.png" alt="Hoteliers Talk" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/8.png" alt="Sambad"  class="img-responsive">
+                </div> 
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/8.png" alt="News 7" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/9.png" alt="PPL News" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/10.png" alt="Dainik Bhaskar" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/11.png" alt="The Samaja" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/12.png" alt="News 18" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Published_Icons/13.png" alt="Kanak News"  class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </section>
+</div> 
+
+
+<!-- Start Published on updated
+    ============================================= -->
+<section class="small-published">
+
+
+    <h2>Published on</h2>
+   
+    <div class="carousel-wrap d-block d-sm-block d-xs-block d-md-none d-lg-none d-xxl-none d-xl-none">
+        <div class="owl-carousel pub">
+            <div class="item items"><img src="assets/Published_Icons/1.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/2.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/3.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/4.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/5.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/6.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/7.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/8.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/9.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/10.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/11.png"></div>
+            <div class="item items"><img src="assets/Published_Icons/12.png"></div>
+        </div>
+    </div>
+    
+
+</section>
+
+
+
+
+
+<!-- Start Recognized by 
+    ============================================= -->
+<!-- <div>
+    <section class="published-on">
+        <div class="container">
+            <h2>Recognized By</h2>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <img src="assets/Recognized_Icons/1.png" alt="The Times of India" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/2.png" alt="Zee Media" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/3.png" alt="ANI News India" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/4.png" alt="Indian Express" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/5.png" alt="Business Standard" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/6.png" alt="Jio TV" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/7.png" alt="Hoteliers Talk" class="img-responsive">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+                    <img src="assets/Recognized_Icons/8.png" alt="Sambad" class="img-responsive"> 
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/9.png" alt="News 7" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/10.png" alt="PPL News" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/11.png" alt="Dainik Bhaskar" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/12.png" alt="The Samaja" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/13.png" alt="News 18" class="img-responsive">
+                </div>
+                <div class="col">
+                    <img src="assets/Recognized_Icons/14.png" alt="Kanak News" class="img-responsive">
+                </div> 
+            </div>
+        </div>
+    </section>
+</div> -->
+
+
+
+<!-- Start Published on 
+    ============================================= -->
+    <div>
+    <section class="published-on">
+        <div class="container site-heading">
+            <h2>Recognized By</h2>
+            <div class="row justify-content-center">
+
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/1.png" alt="The Times of India" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/2.png" alt="Zee Media" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/3.png" alt="ANI News India" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/4.png" alt="Indian Express" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/5.png" alt="Business Standard" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/6.png" alt="Jio TV" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/7.png" alt="Hoteliers Talk" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/8.png" alt="Sambad"  class="img-responsive">
+                </div> 
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/8.png" alt="News 7" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/9.png" alt="PPL News" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/10.png" alt="Dainik Bhaskar" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/11.png" alt="The Samaja" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/12.png" alt="News 18" class="img-responsive">
+                </div>
+                <div class="col-md-2">
+                    <img src="assets/Recognized_Icons/13.png" alt="Kanak News"  class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </section>
+</div> 
+
+
+<!-- Start Published on updated
+    ============================================= -->
+<section class="small-published">
+
+
+    <h2>Recognized By</h2>
+   
+    <div class="carousel-wrap d-block d-sm-block d-xs-block d-md-none d-lg-none d-xxl-none d-xl-none">
+        <div class="owl-carousel pub">
+            <div class="item items"><img src="assets/Recognized_Icons/1.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/2.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/3.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/4.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/5.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/6.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/7.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/8.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/9.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/10.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/11.png"></div>
+            <div class="item items"><img src="assets/Recognized_Icons/12.png"></div>
+        </div>
+    </div>
+    
+
+</section>
+
+
+
+
+
+
+
+<!-- End Recognized by -->
+
+
+
+<!-- updated Published on script  -->
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/owl.carousel.min.js"></script>
+<script>
+    $('.pub').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: [
+            "<i class='fa fa-caret-left'></i>",
+            "<i class='fa fa-caret-right'></i>"
+        ],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },  
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+</script>
+ 
+
+
+
+
+<?php include 'footer.php'; ?>
